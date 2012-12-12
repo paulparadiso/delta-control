2264853e314af10056867c193e5511f87f0f2a6b
Paul Paradiso <paulparadiso@gmail.com>
2012-12-12 17:33:28 -0500

    added playlists template

--- code/delta.py
+++ code/delta.py
@@ -2,7 +2,10 @@ import web
 import redis
 
 urls = (
-	'/', 'index',
+	'/', 'Index',
+	'/master', 'Index',
+	'/control','Command',
+	'/playlists','Playlists',
 )
 
 redis = redis.Redis('localhost')
@@ -10,22 +13,50 @@ redis = redis.Redis('localhost')
 render = web.template.render('templates')
 #print render.hello('world')
 
-class index:
+class Index:
 
 	def GET(self):
 		header = render.header()
 		nav = render.nav()
 		playlist_keys = redis.keys('playlist:*')
-		playlists = ['test1','test2','test3','test4','test5']
+		#playlists = ['test1','test2','test3','test4','test5']
+		playlists = []
 		for i in playlist_keys:
 			playlists.append(i.split(':')[1])
 		return render.master(header, nav, playlists)
-		
-class control:
-	
-	def GET(self):
-		print "play"
 
+class Playlists:
+
+	def GET(self):
+		header = render.header()
+		nav = render.nav()
+		playlists = []
+		playlist_keys = redis.keys('playlist:*')
+		for i in playlist_keys:
+			playlists.append(i.split(':')[1])
+		return render.playlists(header, nav, playlists)
+		
+class Command:
+	
+	def POST(self):
+		#print d
+		params = web.input()
+		if params.cmdtype == 'playlist':
+			self._start_playlist(params.cmd)
+		if params.cmdtype == 'playback':
+			self._control_cmd(params.cmd)
+		if params.cmdtype == 'power':
+			self._set_power(params.cmd)
+			
+	def _start_playlist(self, plist):
+		print "starting playlist - " + plist
+		
+	def _control_cmd(self, cmd):
+		print "sending playback command - " + cmd
+		
+	def _set_power(self, cmd):
+		print "setting power to - " + cmd
+		
 if __name__ == "__main__":	
 	app = web.application(urls, globals())
 	app.run()
--- code/static/js/delta.js
+++ code/static/js/delta.js
@@ -1,9 +1,43 @@
-function sendCmd(cmd){
-	alert(cmd + " pressed.");
+function sendCmd(cmdType, command){
+	//alert(command + " pressed.");
+	$.ajax({
+		url:'control',
+		type:'POST',
+		data:{cmdtype:cmdType,cmd:command}
+	});
 }
 
 function sendPlaylist(){
-	var dd = document.getElementById("playlist-dropdown")
-	var selectedPlaylist = dd.options[dd.selectedIndex].value
-	alert(selectedPlaylist + " selected");
+	var dd = document.getElementById("playlist-dropdown");
+	var selectedPlaylist = dd.options[dd.selectedIndex].value;
+	//alert(selectedPlaylist + " selected");
+	sendCmd('playlist',selectedPlaylist);
+}
+
+function swapTab(currentTab){
+	oldTab = '';
+	newTab = '';
+	if(currentTab == 'view'){
+		oldTab = '#edit-tab';
+		newTab = '#view-tab';
+	} else if(currentTab == 'edit'){
+		oldTab = '#view-tab';
+		newTab = '#edit-tab';
+	}
+	$(oldTab).removeClass('active-tab');
+	$(oldTab).addClass('inactive-tab');
+	$(newTab).removeClass('inactive-tab');
+	$(newTab).addClass('active-tab');
+}
+
+function openPlaylist(plist){
+	var plusSrc = 'static/img/plus.png';
+	var minusSrc = 'static/img/minus.png';
+	var plistImg = '#' + plist + ' img';
+	var imgSrc = $(plistImg).first().attr('src');
+	if(imgSrc == plusSrc){
+		$(plistImg).first().attr('src', minusSrc);
+	} else {
+		$(plistImg).first().attr('src', plusSrc);
+	}
 }
